4805fde18f142ab608159fd0d2a7edc1784da700
unknown <ivicic@IVICIC3.emea.hpqcorp.net>
2014-04-30 22:16:42 +0200

    add support for windows paths

--- clarify.js
+++ clarify.js
@@ -4,10 +4,12 @@
  */
 
 var chain = require('stack-chain');
+var pathSeparator = require('path').sep;
+var stringIndexOf = String.prototype.indexOf;
 
 chain.filter.attach(function (error, frames) {
   return frames.filter(function (callSite) {
     var name = callSite && callSite.getFileName();
-    return (name && name[0] === '/');
+    return (name && stringIndexOf.call(name, sep) !== -1);
   });
 });
