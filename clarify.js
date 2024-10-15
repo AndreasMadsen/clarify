31d81dd60fa73d78867724b83bea0dd0f4b7de86
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2024-10-15 12:13:38 +0200

    Add support for Node.js v20

--- clarify.js
+++ clarify.js
@@ -6,6 +6,6 @@ const sep = require('path').sep;
 chain.filter.attach(function (error, frames) {
   return frames.filter(function (callSite) {
     const name = callSite && callSite.getFileName();
-    return (name && name.includes(sep) && !name.startsWith('internal'));
+    return (name && name.includes(sep) && !(name.startsWith('internal') || name.startsWith('node:internal')));
   });
 });
